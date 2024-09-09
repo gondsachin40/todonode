f0bc6950fceeb99923c502bdfb1d32708de33a16
gondsachin40 <gondsachin2006@gmail.com>
2024-09-09 19:02:59 +0530

    file handling

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fs = require("fs");
-let todos = ["gourav", "sourav", "papita"];
+
+// let todos = ["gourav", "sourav", "papita"];
 const myserver = http.createServer((req, res) => {
   // getting to do
   if (req.url === "/getTODO") {
@@ -9,6 +10,8 @@ const myserver = http.createServer((req, res) => {
         res.end("error handling");
       } else {
         res.write(data);
+        const todosFileData = fs.readFileSync("data.txt", "utf-8");
+        const todos = JSON.parse(todosFileData);
         for (let i = 0; i < todos.length; i++) {
           res.write(
             `${todos[i]} <a href='/deleteTODO?index=${i}'>delete</a> </br>`
@@ -25,7 +28,11 @@ const myserver = http.createServer((req, res) => {
     req.on("end", () => {
       const todo = body.split("=")[1];
       try {
+        // todos.push(todo);
+        const todosFileData = fs.readFileSync("data.txt", "utf-8");
+        const todos = JSON.parse(todosFileData);
         todos.push(todo);
+        fs.writeFileSync("data.txt", JSON.stringify(todos));
         res.writeHead(301, {
           location: "/getTODO",
         });
@@ -37,7 +44,10 @@ const myserver = http.createServer((req, res) => {
   } else if (req.url.includes("/deleteTODO")) {
     const splitt = req.url.split("=");
     let indexx = parseInt(splitt[1]);
+    const todosFileData = fs.readFileSync("data.txt", "utf-8");
+    let todos = JSON.parse(todosFileData);
     todos = todos.filter((x, index) => index != indexx);
+    fs.writeFileSync("data.txt", JSON.stringify(todos));
     res.writeHead(301, {
       location: "/getTODO",
     });
